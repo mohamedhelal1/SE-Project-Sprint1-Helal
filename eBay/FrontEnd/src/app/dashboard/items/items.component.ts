d2b73380b5a9cb690fd6af271031cf5e97707fd8
Mohamed Helal <m.h.helal1997@gmail.com>
2018-03-02 21:01:23 +0200

    first push

---
+++ eBay/FrontEnd/src/app/dashboard/items/items.component.ts
@@ -0,0 +1,82 @@
+import { Component,OnInit } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import { HttpClient } from '@angular/common/http';
+import {Observable} from "rxjs/Observable";
+import {isInteger, isNumber} from "@ng-bootstrap/ng-bootstrap/util/util";
+import { Router, ActivatedRoute } from '@angular/router';
+
+
+@Component({
+    selector: 'app-items',
+    templateUrl: './items.component.html',
+    styleUrls: ['./items.component.scss']
+})
+export class ItemsComponent implements OnInit {
+
+    public ProductsList = [];
+
+    constructor(private http: HttpClient) {
+
+    }
+
+    ngOnInit() {
+        let username = JSON.parse(localStorage.getItem('currentUser')).user.username;
+        this.http.get('http://localhost:3000/api/product/getProducts')
+            .subscribe((res: any) => {
+                this.ProductsList = res.data.filter(ProductsList => ProductsList.sellerName === username);
+            });
+    }
+
+    delete(product): void {
+
+        this.ProductsList = this.ProductsList.filter(Product => Product !== product);
+        this.http.delete('http://localhost:3000/api/product/deleteProduct/' + product._id).subscribe();
+
+
+    }
+
+    create(name, price): void {
+        let username = JSON.parse(localStorage.getItem('currentUser')).user.username;
+        const product = {
+            name: name,
+            price: price,
+            createdAt: Date.now(),
+            updatedAt: Date.now(),
+            sellerName: username
+        }
+        this.ProductsList = this.ProductsList.concat(product);
+        this.http.post('http://localhost:3000/api/product/createProduct/', product).subscribe();
+
+    }
+
+    update(name, price, product) {
+        let username = JSON.parse(localStorage.getItem('currentUser')).user.username;
+        if (name == "") {
+            name = product.name;
+        }
+
+        if (price.toString() == "" || price.isPrototypeOf(Number)) {
+            price = product.price;
+        }
+        const newProduct = {
+            name: name,
+            price: price,
+            createdAt: Date.now(),
+            updatedAt: Date.now(),
+            sellerName: username
+        }
+        this.http.patch('http://localhost:3000/api/product/updateProduct/' + product._id, newProduct).subscribe();
+
+    }
+
+    myFunction(name) {
+        var x = document.getElementById(name);
+        if (x.style.display === "none") {
+            x.style.display = "block";
+        } else {
+            x.style.display = "none";
+        }
+
+    }
+
+}
